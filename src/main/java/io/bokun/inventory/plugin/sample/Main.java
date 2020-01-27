812138055103ee9aa12de306d602ff2661dc7a91
Justas <jbatulevicius@gmail.com>
2020-01-27 07:34:28 +0000

    added monitoring server

--- src/main/java/io/bokun/inventory/plugin/sample/Main.java
+++ src/main/java/io/bokun/inventory/plugin/sample/Main.java
@@ -1,6 +1,7 @@
 package io.bokun.inventory.plugin.sample;
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.nio.file.*;
 import java.util.*;
 import java.util.stream.*;
@@ -11,15 +12,39 @@ import com.google.inject.*;
 import com.google.inject.name.*;
 import io.grpc.*;
 import io.grpc.netty.*;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.ssl.*;
+import io.netty.util.ResourceLeakDetector;
 import io.undertow.*;
 import io.undertow.server.*;
 import io.undertow.server.handlers.*;
 import org.slf4j.*;
 
+import static com.google.common.net.HttpHeaders.CONNECTION;
 import static com.google.inject.Scopes.*;
 import static com.google.inject.name.Names.*;
 import static io.grpc.Metadata.*;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpResponseStatus.CONTINUE;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpUtil.is100ContinueExpected;
+import static io.netty.handler.codec.http.HttpUtil.isKeepAlive;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 import static io.netty.handler.ssl.ClientAuth.*;
 import static io.netty.handler.ssl.SslProvider.*;
 
@@ -49,6 +74,11 @@ public class Main {
      */
     private static final String ENVIRONMENT_PREFIX = "SAMPLE_";
 
+    /**
+     * Monitoring port variable. (Optional. By default will use 8886 port)
+     */
+    public static final String MONITORING_PORT_ENV = ENVIRONMENT_PREFIX + "MONITORING_PORT";
+
     @SuppressWarnings("rawtypes")
     private static final ServerCall.Listener NOOP_LISTENER = new ServerCall.Listener() {};
 
@@ -61,6 +91,13 @@ public class Main {
      * TCP port to use for listening for incoming Inventory Server requests.
      */
     private final int port;
+
+    /**
+     * TCP port to monitor service (optional to use)
+     */
+    @Inject(optional = true)
+    @Named(MONITORING_PORT_ENV)
+    private Integer monitoringPort = 8886;
 
     /**
      * gRPC implementation of the plugin API.
@@ -143,6 +180,16 @@ public class Main {
             Main.this.stop();
             log.error("Server shut down");
         }));
+
+        //start optional monitoring server
+        try {
+            Channel monitoringServerChannel = getOptionalMonitoringServer(monitoringPort).sync().channel();
+            if (monitoringServerChannel != null) {
+                monitoringServerChannel.closeFuture().sync();
+            }
+        } catch (InterruptedException e) {
+            log.error("Monitoring server interrupted", e);
+        }
     }
 
     /**
@@ -229,6 +276,13 @@ public class Main {
                     .start();
             log.info("Started REST service on port {}", server.port);
         }
+
+        //Optional monitoring server
+        Channel monitoringServerChannel = getOptionalMonitoringServer(server.monitoringPort).sync().channel();
+        if (monitoringServerChannel != null) {
+            monitoringServerChannel.closeFuture().sync();
+        }
+
     }
 
     /**
@@ -248,4 +302,52 @@ public class Main {
             binder.bind(Main.class).in(SINGLETON);
         }
     }
+
+    private static ChannelFuture getOptionalMonitoringServer(Integer port) {
+        if (port == null || port == 0) {
+            return null;
+        }
+
+        ResourceLeakDetector.setLevel(ResourceLeakDetector.Level.PARANOID);
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .localAddress(new InetSocketAddress(port))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(new HttpServerCodec());
+                        ch.pipeline().addLast(new HttpObjectAggregator(1048576));
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<FullHttpRequest>() {
+                            @Override
+                            public void channelReadComplete(ChannelHandlerContext ctx) {
+                                ctx.flush();
+                                ctx.close();
+                            }
+
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest msg) {
+                                log.debug("Monitoring request from {} to {}", ctx.channel().remoteAddress(), msg.uri());
+
+                                ByteBuf buf = Unpooled.wrappedBuffer("{\"status\":\"UP\"}".getBytes());
+                                DefaultFullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, buf);
+                                response.headers().add(msg.headers());
+                                response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+                                if (isKeepAlive(msg)) {
+                                    response.headers().set(CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+                                }
+                                if (is100ContinueExpected(msg)) {
+                                    ctx.write(new DefaultFullHttpResponse(HTTP_1_1, CONTINUE));
+                                }
+
+                                ctx.write(response);
+                            }
+                        });
+                    }
+                });
+        ChannelFuture channelFuture = b.bind(port);
+        log.debug("Monitoring server started, listening on {}", port);
+        return channelFuture;
+    }
+
 }
